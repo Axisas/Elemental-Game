ed18a248f96678888e3fa61d6a2d3a6aa98fe703
MSI\Lucas Hoksbergen <lucashoksbergen@gmail.com>
2021-10-29 14:50:40 +0200

    Added prefab for the enemy

---
+++ ElementsGame/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+
+    }
+}
