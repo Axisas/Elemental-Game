76183d4a19212f41ed3e967a951597d63d3a6dd8
MSI\Lucas Hoksbergen <lucashoksbergen@gmail.com>
2021-11-04 16:46:29 +0100

    Added a main menu and a game over screen

--- ElementsGame/Assets/Resources/Scripts/PlayerScripts/PlayerController.cs
+++ ElementsGame/Assets/Resources/Scripts/PlayerScripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -85,8 +86,8 @@ public class PlayerController : MonoBehaviour
 
         if (health <= 0)
         {
-            //Ded
             Debug.Log("RIP");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 
