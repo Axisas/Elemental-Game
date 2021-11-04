2c8ac351876318cc9f0bfc8049a8bcd5f23163ba
MSI\Lucas Hoksbergen <lucashoksbergen@gmail.com>
2021-11-04 17:06:16 +0100

    Added a pause menu

--- ElementsGame/Assets/Resources/Scripts/Menus.cs
+++ ElementsGame/Assets/Resources/Scripts/Menus.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartOverMenu: MonoBehaviour
+public class Menus : MonoBehaviour
 {
+
+    public int nextScene;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + nextScene);
     }
 
     public void ExitGame()
---
+++ ElementsGame/Assets/Resources/Scripts/PausedMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausedMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        GamePaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        GamePaused = true;
+    }
+}
